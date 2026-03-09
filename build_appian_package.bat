d4b28aae2d0ee97ab12b094c56933f3b4811c16d
Dr. Sahebrao Pawar <shpawarsmu@gmail.com>
2026-03-09 22:54:58 +0530

    Add Windows batch file to build Appian package ZIP

---
+++ build_appian_package.bat
@@ -0,0 +1,10 @@
+@echo off
+echo ============================================
+echo  Appian Package Builder - Windows
+echo ============================================
+echo.
+echo Building ZIP from "Test Code  Sandbox" folder...
+echo.
+python build_appian_package.py
+echo.
+pause
